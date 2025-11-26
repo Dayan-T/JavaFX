0f608946de8bb297b877f62e3d2fb9d52de7eab3
Dayan-T <dayan.tomczyk@gmail.com>
2025-11-26 11:07:04 +0100

    JAVAFX

---
+++ src/Launcher.java
@@ -0,0 +1,8 @@
+public class Launcher {
+    public static void main(String[] args) {
+        // Cette classe sert uniquement à lancer l'application
+        // sans que Java ne vérifie les modules JavaFX immédiatement.
+        // C'est l'astuce classique quand on utilise le SDK GluonHQ.
+        Main.main(args);
+    }
+}
