0bb46f377cccb427967c490b72bc6d0629279e78
SaberBerrehili <sabercool04@gmail.com>
2025-11-26 11:53:42 +0100

    Modification de Shoes

--- src/model/Shoes.java
+++ src/model/Shoes.java
@@ -1,37 +1,34 @@
 package model;
 
 public class Shoes extends Product {
-    //Attributs
     private int shoeSize;
 
-    //Constructeur
-    public Shoes(String name, double purprice, double sellprice, int shoeSize){
-        super(name,purprice,sellprice);
+    public Shoes(String name, double purprice, double sellprice, int shoeSize) {
+        super(name, purprice, sellprice);
+        setShoeSize(shoeSize);
+    }
+
+    public int getShoeSize() { return shoeSize; }
+
+    public void setShoeSize(int shoeSize) {
+        if (shoeSize < 36 || shoeSize > 50) {
+            throw new IllegalArgumentException("Pointure invalide (36-50)");
+        }
         this.shoeSize = shoeSize;
     }
 
-    //Getter
-    public int getShoeSize() {
-        return shoeSize;
-    }
-
-    //Setter
-    public void setShoeSize(int shoeSize){
-        try{
-            if (shoeSize <= 36 || shoeSize >= 50 ){
-                throw new IllegalArgumentException("Wrong Shoe size");
-            }
-            this.shoeSize = shoeSize;
-        } catch (IllegalArgumentException e) {
-            System.out.println("Erreur ! La taille doit etre comprise entre 36 et 50");
-            throw e;
-        }
-    }
-
-    //Méthode ToString
     @Override
-    public String toString(){
-        return super.toString() + ", shoeSize = " + shoeSize;
+    public void applyDiscount() {
+        this.discprice = this.sellprice * (1 - SHOES_DISCOUNT);
     }
 
+    @Override
+    public void unApplyDiscount() {
+        this.discprice = 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Pointure: " + shoeSize;
+    }
 }
